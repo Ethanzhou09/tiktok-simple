c733e8f91a4389516402f34e283784dae034c07f
chenz <chenz1999@outlook.com>
2024-07-21 16:06:03 +0800

    添加引用

---
+++ cmd/gateway/handler/user.go
@@ -0,0 +1,17 @@
+package handler
+
+import (
+	"context"
+	"github.com/cloudwego/hertz/pkg/app"
+)
+
+
+func Register(ctx context.Context, c *app.RequestContext){
+
+
+}
+
+func Login(ctx context.Context, c *app.RequestContext){
+	
+
+}
--- cmd/gateway/main.go
+++ cmd/gateway/main.go
@@ -2,18 +2,67 @@ package main
 
 import (
 	"context"
-
-    "github.com/cloudwego/hertz/pkg/app"
-    "github.com/cloudwego/hertz/pkg/app/server"
-    "github.com/cloudwego/hertz/pkg/common/utils"
-    "github.com/cloudwego/hertz/pkg/protocol/consts"
+	"github.com/Ethanzhou09/tiktok-simple/cmd/gateway/handler"
+	"github.com/cloudwego/hertz/pkg/app"
+	"github.com/cloudwego/hertz/pkg/app/server"
+	"github.com/cloudwego/hertz/pkg/common/utils"
+	"github.com/cloudwego/hertz/pkg/protocol/consts"
 )
-func main() {
+
+func registerGroup(hz *server.Hertz) {
+	douyin := hz.Group("/douyin")
+	{
+		user := douyin.Group("/user")
+		{
+			user.GET("/", )
+			user.POST("/register/", handler.Login)
+			user.POST("/login/",handler.Register )
+
+		}
+		// message := douyin.Group("/message")
+		// {
+		// 	message.GET("/chat/", )
+		// 	message.POST("/action/",)
+		// }
+		// relation := douyin.Group("/relation")
+		// {
+		// 	// 粉丝列表
+		// 	relation.GET("/follower/list/", )
+		// 	// 关注列表
+		// 	relation.GET("/follow/list/",)
+		// 	// 朋友列表
+		// 	relation.GET("/friend/list/",)
+		// 	relation.POST("/action/",)
+		// }
+		// publish := douyin.Group("/publish")
+		// {
+		// 	publish.GET("/list/",)
+		// 	publish.POST("/action/",)
+		// }
+		douyin.GET("/feed",)
+		// favorite := douyin.Group("/favorite")
+		// {
+		// 	favorite.POST("/action/",)
+		// 	favorite.GET("/list/",)
+		// }
+		// comment := douyin.Group("/comment")
+		// {
+		// 	comment.POST("/action/",)
+		// 	comment.GET("/list/",)
+		// }
+	}
+}
+
+func InitHertz() *server.Hertz {
     h := server.Default()
+    registerGroup(h)
+    return h
+}
 
-    h.GET("/ping", func(c context.Context, ctx *app.RequestContext) {
-            ctx.JSON(consts.StatusOK, utils.H{"message": "pong"})
-    })
+func main() {
+	hz := InitHertz()
 
-    h.Spin()
+	registerGroup(hz)
+
+    hz.Spin()
 }
