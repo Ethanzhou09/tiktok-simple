d502603c77ccbca916a36d00abd14c86203c5773
chenz <chenz1999@outlook.com>
2024-07-30 22:59:05 +0800

    完善了gateway

--- cmd/gateway/handler/user.go
+++ cmd/gateway/handler/user.go
@@ -3,6 +3,7 @@ package handler
 import (
 	"context"
 	"net/http"
+	"strconv"
 	"github.com/cloudwego/hertz/pkg/app"
 	"tiktok-simple/internal/response"
 	"tiktok-simple/cmd/gateway/rpc"
@@ -57,11 +58,93 @@ func Register(ctx context.Context, c *app.RequestContext){
 }
 
 func Login(ctx context.Context, c *app.RequestContext){
-	
-
+	username := c.Query("username")
+	password := c.Query("password")
+	//校验参数
+	if len(username) == 0 || len(password) == 0 {
+		c.JSON(http.StatusBadRequest, response.Login{
+			Base: response.Base{
+				StatusCode: -1,
+				StatusMsg:  "用户名或密码不能为空",
+			},
+		})
+		return
+	}
+	if len(username) > 32 || len(password) > 32 {
+		c.JSON(http.StatusOK, response.Login{
+			Base: response.Base{
+				StatusCode: -1,
+				StatusMsg:  "用户名或密码长度不能大于32个字符",
+			},
+		})
+		return
+	}
+	req := &user.UserLoginRequest{
+		Username: username,
+		Password: password,
+	}
+	res,err := rpc.Login(ctx,req)
+	if err != nil {
+		c.JSON(http.StatusOK, response.Login{
+			Base: response.Base{
+				StatusCode: -1,
+				StatusMsg:  "登录失败",
+			},
+		})
+		return
+	}
+	c.JSON(http.StatusOK, response.Login{
+		Base: response.Base{
+			StatusCode: 0,
+			StatusMsg:  "登录成功",
+		},
+		UserID: res.UserId,
+		Token: res.Token,
+	})
 }
 
 func UserInfo(ctx context.Context, c *app.RequestContext){
-	
-
+	userId := c.Query("user_id")
+	token := c.Query("token")
+	//校验参数
+	if len(token) == 0 {
+		c.JSON(http.StatusBadRequest, response.UserInfo{
+			Base: response.Base{
+				StatusCode: -1,
+				StatusMsg:  "token已过期",
+			},
+		})
+		return
+	}
+	id, err := strconv.ParseInt(userId, 10, 64)
+	if err != nil {
+		c.JSON(http.StatusOK, response.UserInfo{
+			Base: response.Base{
+				StatusCode: -1,
+				StatusMsg:  "用户id不合法",
+			},
+		})
+		return
+	}
+	req := &user.UserInfoRequest{
+		UserId: id,
+		Token: token,
+	}
+	res ,err := rpc.UserInfo(ctx,req)
+	if err != nil || res.StatusCode==-1{
+		c.JSON(http.StatusOK, response.UserInfo{
+			Base: response.Base{
+				StatusCode: -1,
+				StatusMsg:  "获取用户信息失败",
+			},
+		})
+		return
+	}
+	c.JSON(http.StatusOK, response.UserInfo{
+		Base: response.Base{
+			StatusCode: 0,
+			StatusMsg:  res.StatusMsg,
+		},
+		User: res.User,
+	})
 }
--- cmd/gateway/main.go
+++ cmd/gateway/main.go
@@ -4,11 +4,27 @@ import (
 	"tiktok-simple/cmd/gateway/rpc"
 	"tiktok-simple/cmd/gateway/router"
 	"github.com/cloudwego/hertz/pkg/app/server"
+	"github.com/spf13/viper"
 )
 
+type Server struct{
+	Name string `mapstructure:"name"`
+	Host string `mapstructure:"host"`
+	Port string `mapstructure:"port"`
+}
+
+type Cfg struct {
+	Server Server `mapstructure:"server"`
+}
 
 func InitHertz() *server.Hertz {
-	h := server.Default()
+	v := viper.New()
+	v.SetConfigFile("../../cfg/gateway.yml")
+	v.ReadInConfig()
+	cfg := Cfg{}
+	v.Unmarshal(&cfg)
+	servercfg := cfg.Server
+	h := server.Default(server.WithHostPorts(servercfg.Host + ":" + servercfg.Port))
 	return h
 }
 
